bc92646e6dd75c1024a595822b107b1ebbed5815
student8 <student8@S7N-PROGLANG-43.pcoip.ki.agh.edu.pl>
2017-02-25 15:46:49 +0100

    Aktualka

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,27 +3,73 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Map;
+import java.util.Random;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	HashMap<Product,Integer> products;
+	private Map<Product,Integer> products;
+	private static long nextID;
+	private long ID;
 	
 	public Invoice() {
+		ID=nextID;
+		nextID++;
 		products=new HashMap<Product,Integer>();
+	}
+	public static void resetID() {
+		nextID=1;
+	}
+	
+	public String printInvoice() {
+		StringBuilder s=new StringBuilder("Faktura nr "+ID+"\n");
+		s.append("***********************\n");
+		
+		for (Product p:products.keySet()) {
+			
+			String ptype=p.getClass().getSimpleName();
+			String pname=p.getName();
+			String pprice=p.getPrice().toString();
+			String pquantity=products.get(p).toString();
+			s.append(ptype+" "+pname+" "+pprice+" "+pquantity+"\n");
+		}
+		s.append("***********************\n");
+		s.append("Wartosc netto:"+getNetTotal()+"\n");
+		s.append("Podatek:"+getTax()+"\n");
+		s.append("Wartosc brutto:"+getTotal()+"\n");
+		s.append("Liczba pozycji:"+products.size()+"\n");
+		s.append("***********************\n");
+		
+		return s.toString();
 	}
 	
 	public void addProduct(Product product) {
-		products.put(product,1);
+		addProduct(product,1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
+		if (product==null) throw new IllegalArgumentException();
 		if (quantity<1) throw new IllegalArgumentException();
-		products.put(product,quantity);
+		boolean duplicate=false;
+		Product other=null;
+		for (Product p:products.keySet()) {
+			if (p.getName().equals(product.getName())) {
+				duplicate=true;
+				other=p;
+			}
+		}
+		if (duplicate) {
+			int q=products.get(other);
+			q=q+quantity;
+			products.put(other, quantity);
+		}
+		else {
+			products.put(product,quantity);
+		}
 	}
 
-	public BigDecimal getSubtotal() {
+	public BigDecimal getNetTotal() {
 		BigDecimal sum=BigDecimal.ZERO;
 		for (Product prod:products.keySet()) {
 			sum=sum.add(prod.getPrice().multiply(new BigDecimal(products.get(prod))));
@@ -40,8 +86,14 @@ public class Invoice {
 	}
 
 	public BigDecimal getTotal() {
-		BigDecimal subt=getSubtotal();
+		BigDecimal subt=getNetTotal();
 		BigDecimal total=subt.add(getTax());
 		return total;
 	}
+	
+	public long getID() {
+		// TODO Auto-generated method stub
+		return ID;
+	}
+	
 }
--- src/test/java/pl/edu/agh/mwo/invoice/InvoiceTest.java
+++ src/test/java/pl/edu/agh/mwo/invoice/InvoiceTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import pl.edu.agh.mwo.invoice.Invoice;
@@ -16,11 +19,109 @@ public class InvoiceTest {
 	private static final String PRODUCT_1 = "Product 1";
 	private static final String PRODUCT_2 = "Product 2";
 	private static final String PRODUCT_3 = "Product 3";
-
+	
+	@Before
+	public void clean() {
+		Invoice.resetID();
+	}
+	
+	@Test
+	public void testInvoiceHasNumberGreaterThanZero() {
+		Invoice invoice=createEmptyInvoice();
+		Assert.assertThat(invoice.getID(), Matchers.greaterThan((long)0));
+	}
+	@Test
+	
+	public void testDifferentNumbers() {
+		Invoice inv1=createEmptyInvoice();
+		Invoice inv2=createEmptyInvoice();
+		Assert.assertNotEquals(inv1.getID(), inv2.getID());
+	}
+	
+	@Test
+	public void testPrintedInvoiceHasNumber() {
+		Invoice invoice=createEmptyInvoice();
+		String printed=invoice.printInvoice();
+		String invoiceNo=Long.toString(invoice.getID());
+		Assert.assertThat(printed, Matchers.containsString(invoiceNo));
+	}
+	
+	@Test
+	public void testInvoiceHasProductName() {
+		Invoice invoice=createEmptyInvoice();
+		Product milk=new DairyProduct("mleko", new BigDecimal(3.33));
+		invoice.addProduct(milk);
+		String printed=invoice.printInvoice();
+		Assert.assertThat(printed,Matchers.containsString("mleko"));
+	}
+	@Test
+	public void testInvoiceHasProductPrice() {
+		Invoice invoice=createEmptyInvoice();
+		Product milk=new DairyProduct("mleko", new BigDecimal(3.33));
+		invoice.addProduct(milk);
+		String printed=invoice.printInvoice();
+		Assert.assertThat(printed,Matchers.containsString("3.33"));
+	}
+	@Test
+	public void testInvoiceHasProductQuantity() {
+		Invoice invoice=createEmptyInvoice();
+		Product milk=new DairyProduct("mleko", new BigDecimal(3.33));
+		invoice.addProduct(milk,888);
+		String printed=invoice.printInvoice();
+		Assert.assertThat(printed,Matchers.containsString("888"));
+	}
+	@Test
+	public void testNoDuplicates() {
+		Invoice invoice=createEmptyInvoice();
+		Product milk=new DairyProduct("mleko", new BigDecimal(3));
+		Product milk2=new DairyProduct("mleko", new BigDecimal(3));
+		
+		invoice.addProduct(milk,3);
+		invoice.addProduct(milk2,3);
+		String printed=invoice.printInvoice();
+		Assert.assertThat(printed,Matchers.containsString("Liczba pozycji:1"));
+	}
+	
+	@Test
+	public void testInvoiceHasNumberofPositions() {
+		Invoice i=createEmptyInvoice();
+		Product p1=new DairyProduct("P1",new BigDecimal(1));
+		Product p2=new DairyProduct("P2",new BigDecimal(1));
+		Product p3=new DairyProduct("P",new BigDecimal(1));
+		i.addProduct(p1);
+		i.addProduct(p2);
+		i.addProduct(p3);
+		String printed=i.printInvoice();
+		Assert.assertThat(printed, Matchers.containsString("3"));
+	}
+	
+	
+	@Test
+	public void testNextInvoiceHasSubsequentNumbers() {
+		Invoice inv1=createEmptyInvoice();
+		Invoice inv2=createEmptyInvoice();
+		Assert.assertEquals(1, inv2.getID()-inv1.getID());
+	}
+	
+	@Test
+	public void testFirstInvoiceId() {
+		Invoice inv=createEmptyInvoice();
+		assertEquals(1,inv.getID());
+	}
+	
+	@Test
+	public void testThirdInvoiceId() {	
+		createEmptyInvoice();
+		createEmptyInvoice();
+		Invoice inv=createEmptyInvoice();
+		assertEquals(3,inv.getID());
+	}
+	 
+	
 	@Test
 	public void testEmptyInvoiceHasEmptySubtotal() {
 		Invoice invoice = createEmptyInvoice();
-		assertBigDecimalsAreEqual(BigDecimal.ZERO, invoice.getSubtotal());
+		assertBigDecimalsAreEqual(BigDecimal.ZERO, invoice.getNetTotal());
 	}
 
 	@Test
@@ -39,7 +140,7 @@ public class InvoiceTest {
 	public void testInvoiceHasTheSameSubtotalAndTotalIfTaxIsZero() {
 		Invoice invoice = createEmptyInvoice();
 		invoice.addProduct(createTaxFreeProduct(), 1);
-		assertBigDecimalsAreEqual(invoice.getSubtotal(), invoice.getTotal());
+		assertBigDecimalsAreEqual(invoice.getNetTotal(), invoice.getTotal());
 	}
 
 	@Test
@@ -48,7 +149,7 @@ public class InvoiceTest {
 		invoice.addProduct(createTaxFreeProduct(), 1);
 		invoice.addProduct(createOtherProduct(), 1);
 		invoice.addProduct(createDairyProduct(), 1);
-		assertBigDecimalsAreEqual("259.99", invoice.getSubtotal());
+		assertBigDecimalsAreEqual("259.99", invoice.getNetTotal());
 	}
 
 	@Test
@@ -75,7 +176,7 @@ public class InvoiceTest {
 		invoice.addProduct(createTaxFreeProduct(), 3); // Subtotal: 599.97
 		invoice.addProduct(createOtherProduct(), 2); // Subtotal: 100.00
 		invoice.addProduct(createDairyProduct(), 4); // Subtotal: 40.00
-		assertBigDecimalsAreEqual("739.97", invoice.getSubtotal());
+		assertBigDecimalsAreEqual("739.97", invoice.getNetTotal());
 	}
 
 	@Test
